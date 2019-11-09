c874894f969be463e0ee2576986522bf2eff4329
garganjohn <garganjohn@gmail.com>
2019-11-09 18:21:30 -0500

    Basic String manipulation

---
+++ Giraffe.py
@@ -0,0 +1,12 @@
+phrase = "Giraffe Academy"
+
+print("Giraffe\nAcademy")
+print("Giraffe \"Academy\"")
+print(phrase.lower())
+print(phrase.upper().isupper())
+print(phrase.isupper())
+print(phrase.islower())
+print(len(phrase))
+print(phrase.index("ffe"))  # Returns index of given parameter
+
+print(phrase.replace("Giraffe", "Elephant"))
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
-character_name = "John"
-character_age = "35"
+character_name = "Tom"
+character_age = "50.768442"
+is_male = True
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
 print("He really liked the name " + character_name + ", ")
