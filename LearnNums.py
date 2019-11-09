9670701b71c168ec382694db4540d98d05c78503
garganjohn <garganjohn@gmail.com>
2019-11-09 18:31:43 -0500

    Manipulating numbers

--- LearnNums.py
+++ LearnNums.py
@@ -1,2 +1,12 @@
+from math import *
 
-print(2)
+my_num = 5
+neg_num = -9
+print(abs(neg_num))
+print(pow(4, 6))  # 4^6
+print(max(4, 6))  # prints bigger number
+print(round(3.2))  # normal rounding
+print(floor(3.7))  # rounds down
+print(ceil(3.3))  # rounds up
+print(sqrt(my_num))  # prints square root
+print(str(my_num) + " my fave number.")  # just like .toString() in java
